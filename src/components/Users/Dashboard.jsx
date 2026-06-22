9a9f4931155c5718115f940cbfcdf9964675b22d
Saksham <sakshamv108@gmail.com>
2026-06-22 14:03:29 +0530

    first commit

---
+++ src/components/Users/Dashboard.jsx
@@ -0,0 +1,14 @@
+import TransactionChart from "../Transactions/TransactionChart";
+import Totals from "../Transactions/Totals";
+
+const Dashboard = () => {
+    return (
+        <>
+        <Totals/>
+        <TransactionChart/>
+        </>
+
+    )
+}
+
+export default Dashboard;
